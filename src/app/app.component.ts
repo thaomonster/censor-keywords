af56c44f424abd4c9591b751804ccc5991141f81
Thao Ma <MaThao@gmail.com>
2021-05-17 13:43:07 -0600

    Move html into app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <header>
+      <h1>Censor Keyword</h1>
+    </header> 
+    <main>
+    <div>
+      <b>Censor keywords goes here</b>
+      <input type='text' placeholder="Example 'protection', 'important', 'safety' " />
+      <textarea type ='text' placeholder='Copy and paste document here'></textarea>
+      <button>Submit</button>
+    </div>
+    <div>
+      Results go here
+    </div>
+    </main>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
