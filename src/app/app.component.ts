209b44b8f92c7ea2ddac77ecce7bc74e2bc22b58
Thao Ma <MaThao@gmail.com>
2021-05-17 14:16:49 -0600

    Add basic onClick funtion to button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,22 @@ import { Component } from '@angular/core';
       <h1>Censor Keyword</h1>
     </header> 
     <main>
-    <div>
-      <b>Censor keywords goes here</b>
-      <input type='text' placeholder="Example 'protection', 'important', 'safety' " />
-      <textarea type ='text' placeholder='Copy and paste document here'></textarea>
-      <button>Submit</button>
-    </div>
-    <div>
-      Results go here
-    </div>
-    </main>
-  `,
+      <div>
+        <b>Censor keywords goes here</b>
+        <input type='text' placeholder="Example 'protection', 'important', 'safety' " />
+        <textarea type ='text' placeholder='Copy and paste document here'></textarea>
+        <button (click)='onClick()'>Submit</button>
+      </div>
+      <div>
+        Results go here
+      </div>
+    </main>`,
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'censor-keywords';
+  title = 'Censor Keywords';
+  onClick() {
+    console.log('working')
+  }
 }
