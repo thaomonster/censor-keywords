6761bc781790859dc6dba9ef752bea2ccb6c96ae
Thao Ma <MaThao@gmail.com>
2021-05-18 16:20:11 -0600

    Add functionality to censor words

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
-import { Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +9,27 @@ import { Validators } from '@angular/forms';
 
 export class AppComponent {
   title = 'Censor Keywords';
+  resultDoc=''
   inputForm = new FormGroup({
     keywordInput: new FormControl('', Validators.required),
-    documentInput: new FormControl('', Validators.required)
+    documentInput: new FormControl('', Validators.required),
   });
-  
 
-  onSubmit() {
-    console.log(this.inputForm.value)
+  onSubmit = () => {
+    const keyword = this.inputForm.get('keywordInput')!.value
+    const document = this.inputForm.get('documentInput')!.value
+
+    if (document.includes(keyword)) {
+      const result = document.replaceAll(keyword, 'XXXX')
+      this.resultDoc = result
+    } else {
+      console.log('not matching word')
+    }
+
+    //use replace to replace all words that match the keyword
+    //use a condtional to do this
+
+    // console.log(this.inputForm.get('keywordInput')!.value)
+    // console.log(this.inputForm.get('documentInput')!.value)
   };
 }
